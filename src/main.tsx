58adac2c48e1e5d7aadc62aa47b18a202ba1155f
theketofeed <theketofeed.ceo@gmail.com>
2026-05-08 00:57:23 +0100

    feat: integrate Sentry and update PostHog initialization in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,26 @@
+import * as Sentry from '@sentry/react'
+import posthog from 'posthog-js'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AuthProvider } from './context/AuthContext'
 import './index.css'
-import posthog from 'posthog-js'
+
+Sentry.init({
+  dsn: 'your_dsn_here',
+  environment: 'production',
+  tracesSampleRate: 0.1,
+})
 
 posthog.init('phc_AXP8PV69MvddHZnRbyuItedeuFZ9n9lznYo0uotTbsq', {
-	api_host: 'https://eu.i.posthog.com',
-	capture_pageview: true
+  api_host: 'https://eu.i.posthog.com',
+  capture_pageview: true
 })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<AuthProvider>
-			<App />
-		</AuthProvider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 )
